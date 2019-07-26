98e478ae53cc54e9577a7d36861c4e6c3d77a20d
hmt114 <hmt_114@163.com>
2019-07-26 19:41:13 +0800

    第一次提交淘淘

---
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/web/TaotaoManagerWebApplication.java
@@ -0,0 +1,13 @@
+package com.taotao.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaotaoManagerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaotaoManagerWebApplication.class, args);
+    }
+
+}
